71a8171b1dbd8cb53ec517dc9f77dccb2b0a4463
M. Haris <muhammadharisaslam1@gmail.com>
2023-06-24 21:52:46 +0500

    clear js css html

--- js/script.js
+++ js/script.js
@@ -1,42 +1,25 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 5) {
-          $(".nav-bar").css("background" , "#f2f3f7");
-          $(".nav-bar h1").css("color" , "#000000 ");
-          $(".nav-bar-icon").css("display" , "flex");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 5) {
+      $(".nav-bar").css("background", "#f2f3f7");
+      $(".nav-bar h1").css("color", "#000000 ");
+      $(".nav-bar-icon").css("display", "flex");
 
-        }
-  
-        else{
-            $(".nav-bar").css("background" , "transparent");
-            $(".nav-bar h1").css("color" , "#ffffff");
-            $(".nav-bar-icon").css("display" , "none");
+    }
+    else {
+      $(".nav-bar").css("background", "transparent");
+      $(".nav-bar h1").css("color", "#ffffff");
+      $(".nav-bar-icon").css("display", "none");
 
-        }
-    })
+    }
   })
+})
 
-  // $(document).ready(function(){
-  //   $(window).scroll(function(){
-  //       var scroll = $(window).scrollTop();
-  //       if (scroll > 400) {
+window.ityped.init(document.querySelector('.atyped'), {
+  strings: ['Breaking The Traditional Mold Of Human-Technology Interaction', 'Revolutionizing Input Technology, Unleashing Creativity'],
+  typeSpeed: 100,
+  BackSpeed: 8,
+  loop: true
 
-  //         $(".video").css("display" , "none");
-  //       }
-  
-  //       else{
-
-  //           $(".video").css("display" , "block");
-  //       }
-  //   })
-  // })
-
-
-  window.ityped.init(document.querySelector('.atyped'), {
-    strings: ['Breaking The Traditional Mold Of Human-Technology Interaction','Revolutionizing Input Technology, Unleashing Creativity'],
-    typeSpeed: 100,
-    BackSpeed: 8,
-    loop: true
-
-  })
+})
